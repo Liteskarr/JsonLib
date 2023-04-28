b7857886c47d230c4170000fb53dfb64f9a2283a
Vladimir Popov <398501z3@gmail.com>
2023-04-29 01:15:18 +0300

    feature: File with all includes

---
+++ src/Json.h
@@ -0,0 +1,12 @@
+#ifndef JSONPARSER_BJSON_H
+#define JSONPARSER_BJSON_H
+
+#include "JsonException.h"
+#include "JsonFunctions.h"
+#include "JsonObject.h"
+#include "JsonParser.h"
+#include "JsonFormatter.h"
+#include "JsonParserSettings.h"
+#include "JsonFormatterSettings.h"
+
+#endif

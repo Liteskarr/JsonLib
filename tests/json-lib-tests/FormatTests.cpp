f89b309b4ebe2c9734bcb65ff48b949d53015a07
Vladimir Popov <398501z3@gmail.com>
2023-05-02 15:59:39 +0300

    feature: added yet another format test

--- tests/json-lib-tests/FormatTests.cpp
+++ tests/json-lib-tests/FormatTests.cpp
@@ -11,8 +11,23 @@ TEST(FormatTests, FormatTest1) {
                                         "}\n";
     const int32_t int1 = 32;
     auto obj = JsonObject::MakeObject();
-    auto& obj_map = obj.AsObject();
+    auto &obj_map = obj.AsObject();
     obj_map["hello"] = JsonObject::MakeInt(int1);
     obj_map["world"] = JsonObject::MakeString("string");
     ASSERT_EQ(ToString(obj, {.PrettyFormat = true}), expected_string);
+}
+
+TEST(FormatTests, FormatTest2) {
+    const std::string expected_string = "[1,null,{\"hello\":\"world\"},false,\"text\"]";
+    std::vector<JsonObject> v = {
+            JsonObject::MakeInt(1ll),
+            JsonObject::MakeNull(),
+            JsonObject::MakeObject(*new std::map<std::string, JsonObject>{
+                    {"hello", JsonObject::MakeString("world")}
+            }),
+            JsonObject::MakeBool(false),
+            JsonObject::MakeString("text")
+    };
+    auto obj = JsonObject::MakeArray(v);
+    ASSERT_EQ(ToString(obj, {.PrettyFormat = false}), expected_string);
 }
